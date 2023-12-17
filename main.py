b65e9d58b001e9886f8139d36112e10d6b218222
fbn776 <fbn776@gmail.com>
2023-12-17 18:27:43 +0530

    Code for code to html for record submission

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 import argparse
 from mkpdf import get_pdfs
 from list_gen import generate_files_list
-
+import html
 # CLI Arguments
 parser = argparse.ArgumentParser()
 parser.add_argument("root_folder", type=str, help="Name of the root folder that contains the file to convert")
@@ -16,7 +16,7 @@ args = parser.parse_args()
 # Config
 ROOT = args.root_folder
 include_exts = args.include
-output_path = args.output
+output_filename = args.output
 
 # If `--include` starts with `.`, then remove it;
 for i, j in enumerate(include_exts):
@@ -28,6 +28,40 @@ if not os.path.isdir(ROOT):
     sys.exit(2)
 
 path_list = generate_files_list(ROOT, include_exts)
-get_pdfs(ROOT, path_list, output_path)
+
+str = ""
+for item in path_list:
+    with open(item, "r") as f:
+        str += f"""
+            <h1 align="center">Title</h1>
+            <h3>Aim:</h3>
+            <h3>Algorithm:</h3>
+            <h3>Program Development:</h3>
+            <pre>{html.escape(f.read())}</pre>
+            <h3>Output:</h3>
+            <br/><div class="page-break"></div>
+        """
+
+with open(output_filename, "w") as f:
+    f.write("""
+        <html>
+            <head>
+                <style>
+                    body {
+                        font-size: 14px;
+                    }
+                    @media print {
+                        .page-break {
+                            page-break-before: always;
+                        }
+                    }
+                </style>
+            </head>
+            <body>
+    """ + str + """
+            </body>
+        </html>
+    """)
+
 
 print(f"Converted {len(path_list)} files to pdf")
--- mkpdf.py
+++ mkpdf.py
@@ -2,30 +2,20 @@ from fpdf import FPDF
 import os
 
 
-def create_pdf(save_loc: str, filename: str, text: str):
-    pdf = FPDF()
-    pdf.add_page()
-
-    # Set font and position
-    pdf.set_font("Arial", size=12)
-    pdf.multi_cell(200, 5, txt=text, align="L")
-
-    # Check if directory exists, create it if not
-    if not os.path.exists(save_loc):
-        os.makedirs(save_loc)
-
-    last_dot_index = filename.rindex(".")
-    pdf.output(os.path.join(save_loc, filename[:last_dot_index] + ".pdf"))
-
-
 def get_pdfs(root: str, path_list: list[str], output: str):
     root = os.path.abspath(root)
+    str = ""
     for item in path_list:
         rel_path = os.path.relpath(item, root)
         txt = read_file(item)
         file_name = os.path.basename(rel_path)
         loc_path = os.path.join(output, os.path.dirname(rel_path))
-        create_pdf(loc_path, file_name, txt)
+
+        str += read_file(file_name)
+        # create_pdf(loc_path, file_name, txt)
+
+    with open("Output.html", "w") as out:
+        out.write(str)
 
 
 def read_file(filename: str):
